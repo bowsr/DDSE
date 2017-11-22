d205dbbed8889aceab1e6c7001c021de58fee597
bowsr <bowserm@outlook.com>
2017-11-22 11:30:38 -0800

    Bumped version for checker.

--- src/resource/Resources.java
+++ src/resource/Resources.java
@@ -2,7 +2,7 @@ package resource;
 
 public class Resources {
 
-    public static final String PROGRAM_VERSION = "1.40";
+    public static final String PROGRAM_VERSION = "1.41";
     public static final String GITHUB_REPO_RELEASES_URI = "https://github.com/bowsr/DDSE/releases/tag/";
 
 }
